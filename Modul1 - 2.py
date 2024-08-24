d1923d5d0f0187f996b79a688d7157fb32b97692
Nik084 <regul1200@gmail.com>
2024-08-24 22:34:48 +0300

    Add files via upload

---
+++ Modul1 - 2.py
@@ -0,0 +1,5 @@
+hometasks_total = 12 # Количество выполненных ДЗ
+hours_total = 1.5 # Количество затраченных часов
+studycourse_name = 'Phyton' # Название курса
+onetask_time = hours_total/hometasks_total # Время на одно задание
+print ('Курс:', studycourse_name + ', всего задач: ' + str(hometasks_total) + ', затрачено часов: ' + str(hours_total) + ', среднее время выполнения', onetask_time, 'часа.')
