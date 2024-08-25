ac90480438305d47a28da900041399ae3a226755
Nik084 <regul1200@gmail.com>
2024-08-25 20:47:45 +0300

    Add files via upload

--- Modul1 - 5.py
+++ Modul1 - 5.py
@@ -3,9 +3,13 @@ print ('Immutable tuple:', immutable_var)
 immutable_var [0][1] = immutable_var [0][1].replace('y', 'wo') # изменение содержимого списка, входящего в кортежь
 immutable_var [0][1] = immutable_var [0][1].upper() # изменение регистра данных из списка, входящего в кортежь
 print ('Immutable tuple:', immutable_var)
-mutable_list = ['animal', 'street', 'tiger']
+mutable_list = ['animal', 'street', 'tiger', 1988, 'snake']
 print ('Mutable list:', mutable_list)
-mutable_list.remove('street')
-mutable_list.append('leo')
+mutable_list[1] = 'leo'
+mutable_list.append('panther')
+mutable_list.remove(1988)
+mutable_list.remove('snake')
+mutable_list[0] = 'cats'
 print ('Mutable list:', mutable_list)
-print (mutable_list [0].upper() + 'S:', str(mutable_list [1:3]))
+# последняя строка вне задания. Сделана для повторения и закрепления пройденного.
+print (mutable_list [0].upper() + ':', str(mutable_list [1]).title() + ',', str(mutable_list[2]).title() + ',', str(mutable_list[3]).title())
